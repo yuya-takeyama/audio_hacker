0d9c3cdda0d45774480bfe675b5aed50cf7ab415
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-02-02 21:02:38 +0900

    Implement #files

--- lib/audio_hacker/directory_definition.rb
+++ lib/audio_hacker/directory_definition.rb
@@ -6,5 +6,22 @@ module AudioHacker
       @path = path
       @pattern = pattern
     end
+
+    def files
+      @loaded_files ||= load_files
+    end
+
+    private
+      def load_files
+        files = []
+
+        Dir.chdir(@path) do
+          files = Dir.glob(@pattern).map do |file|
+            File.join(@path, file)
+          end
+        end
+
+        files
+      end
   end
 end
